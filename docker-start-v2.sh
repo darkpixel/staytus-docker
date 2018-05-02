ea70817daab2dc7cd5afa39ad1fa8841f0b3d68b
Aaron C. de Bruyn <aaron@heyaaron.com>
2018-05-01 19:38:06 -0700

    Add a flag to initialize the DB.  Django has *one* command--migrate.  Why does this app have two?  Bad for repeatable idempotent deploys.

--- docker-start-v2.sh
+++ docker-start-v2.sh
@@ -1,4 +1,10 @@
 envsubst < /app/config/database.example.yml > /app/config/database.yml
 envsubst < /app/config/environment.example.yml > /app/config/environment.yml
+
+if [ -n "${DB_INIT+set}" ]; then
+    echo '$DB_INIT was set, initializing database'
+    bundle exec rake staytus:build staytus:install
+fi
+
 bundle exec rake staytus:build staytus:upgrade
 bundle exec foreman start
